3b7a1376947410ea8f034edb6765b8c65df99164
blackjack238 <118196030+blackjack238@users.noreply.github.com>
2023-04-26 18:09:37 +0300

    зробив порект на gradle

---
+++ src/main/java/org/example/Name.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Name {
+    public Name(String name, String lastName) {
+        Name = name;
+        LastName = lastName;
+    }
+
+    private String Name;
+    private String LastName;
+}
